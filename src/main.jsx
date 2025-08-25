632e44af63488999fa532068a566ded443b5c0f7
Natasha Nicole Ejercito <nnejercito@myseneca.ca>
2025-08-25 19:04:23 -0400

    portfolio ✨

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
       <SpeedInsights />
-      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
