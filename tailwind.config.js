2064c24eeb97ad1ea6c4de532eead6966c68621c
Natasha Nicole Ejercito <nnejercito@myseneca.ca>
2025-08-25 19:18:28 -0400

    portfolio ✨

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // or wherever your components are
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
